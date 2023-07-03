de682e1d02c145072d5375720e073f8d27ba9df0
ali591195 <ali.zahid3428@gmail.com>
2023-07-03 17:46:25 +0500

    commit

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,3 +1,5 @@
+// A general middleware for validating request's body. It takes a function which contains validation logic and also return the error if validation fails otherwise send a falsy value
+// It takes the return value and if it is an error, it send status of 400. Otherwise call the next middleware.
 module.exports = (validator) => {
     return async (req, res, next) => {
         const error = await validator(req.body)
